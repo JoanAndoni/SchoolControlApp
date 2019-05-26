ec3af6cbac64e6f0162a285b49fcea1297a42b93
JoanAndoni <mcr_joan@hotmail.com>
2019-05-26 07:32:59 -0500

    Navbar responsive Full

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -12,8 +12,19 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 export class NavbarComponent implements OnInit {
   isResponsive: boolean = false;
   showDropdown: boolean = false;
-  showButton: boolean = true;
   innerWidth: any;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth;
+    if (this.innerWidth < 995) {
+      this.isResponsive = true;
+      this.showDropdown = false;
+    } else {
+      this.isResponsive = false;
+      this.showDropdown = false;
+    }
+  }
 
   constructor(
     private authService: AuthService,
@@ -31,10 +42,8 @@ export class NavbarComponent implements OnInit {
   makeDropdown() {
     if (this.showDropdown) {
       this.showDropdown = false;
-      this.showButton = true;
     } else if (!this.showDropdown) {
       this.showDropdown = true;
-      this.showButton = false;
     }
   }
 
